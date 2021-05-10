17b7a4e6007af7f5ea9f969f7d26aadec208b8e7
ZarryMyles <mahanth2709@gmail.com>
2021-05-10 23:39:38 +0530

    Added-tailwind-css-compatibility

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div class="p-6 max-w-sm mx-auto bg-dark rounded-xl shadow-md flex items-center space-x-4">
+      <div>
+        <div class="text-xxl font-bold text-black">Genesis Design</div>
+        <p class="text-yellow-500 font-bold">
+          Website Under Development <br /> Please wait :0
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
