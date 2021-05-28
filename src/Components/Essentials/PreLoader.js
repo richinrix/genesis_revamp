47f14bcba7c5be0d10ea7f483aa30ed6d8749652
richinrix <richinvishal@gmail.com>
2021-05-29 02:01:25 +0530

    preloader v0.1

--- src/Components/Essentials/PreLoader.js
+++ src/Components/Essentials/PreLoader.js
@@ -3,15 +3,17 @@ import "../CSS/preloader.css";
 
 function PreLoader() {
   return (
-    <div id="preLoader" className="plContainer w-screen md:w-auto h-screen">
-      <div class="ball ball-one"></div>
-      <div class="ball ball-two"></div>
-      <div class="ball ball-three"></div>
-      <div class="ball ball-four"></div>
-      <div class="shadow shadow-one"></div>
-      <div class="shadow shadow-two"></div>
-      <div class="shadow shadow-three"></div>
-      <div class="shadow shadow-four"></div>
+    <div className="h-screen flex items-center">
+      <div className="plContainer md:mx-auto  md:pl-0 ml-20 pl-2 my-auto">
+        <div class="ball ball-one"></div>
+        <div class="ball ball-two"></div>
+        <div class="ball ball-three"></div>
+        <div class="ball ball-four"></div>
+        <div class="shadow shadow-one"></div>
+        <div class="shadow shadow-two"></div>
+        <div class="shadow shadow-three"></div>
+        <div class="shadow shadow-four"></div>
+      </div>
     </div>
   );
 }
