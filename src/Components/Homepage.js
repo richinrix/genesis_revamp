08febe995cb644fddcfffccaf669951c845ccde9
ZarryMyles <mahanth2709@gmail.com>
2021-07-15 16:52:36 +0530

    New vid links

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -18,8 +18,8 @@ function Homepage({ toggleLoading }) {
             // Changing source of video depending on size of the screen
             src={
               window.innerWidth > 700
-                ? "https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL3YvcyFBb3FPVmtqSmQwMzNibHNZRHRYdEtZMGlOeVE_ZT1KeW51a3I/root/content"
-                : "https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL3YvcyFBb3FPVmtqSmQwMzNiM3lfa09EVlhzeDVuQ00_ZT1ZMmVOVXY/root/content"
+                ? "https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBb3FPVmtqSmQwMzNkVk1lM3ZmM1NxNWhrdWs_ZT1oOVJpQTY/root/content"
+                : "https://api.onedrive.com/v1.0/shares/u!aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBb3FPVmtqSmQwMzNkTUFEQ0d4aTg0R3ViS0U_ZT1vMURnVno/root/content"
             }
             type="video/mp4"
           />
